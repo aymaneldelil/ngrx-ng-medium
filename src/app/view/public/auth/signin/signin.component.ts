6d406cde1f7ac6eb71b507a69a97cf44316e38d1
aymanyori <aymanyori00@gmail.com>
2023-02-26 22:12:15 +0200

    Signin Component

--- src/app/view/public/auth/signin/signin.component.ts
+++ src/app/view/public/auth/signin/signin.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  public signin_fg!: FormGroup;
+  constructor(private _fb: FormBuilder) {}
+  //---------------------------------------------------------------------------------------------------------------------------------------------
 
   ngOnInit(): void {
+    this.signinForm();
   }
-
+  //---------------------------------------------------------------------------------------------------------------------------------------------
+  private signinForm(): void {
+    this.signin_fg = this._fb.group({
+      email: [
+        null,
+        Validators.required,
+        Validators.pattern(/[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+      ],
+      password: [null, Validators.required],
+    });
+  }
+  //---------------------------------------------------------------------------------------------------------------------------------------------
+  public formSubmit() {}
 }
